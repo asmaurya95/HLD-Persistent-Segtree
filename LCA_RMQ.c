fe885434e427541810126115d76122ff370fdcfc
Ashutosh Maurya <asmaurya95@users.noreply.github.com>
2017-11-02 22:56:22 +0530

    Update LCA_RMQ.c

--- LCA_RMQ.c
+++ LCA_RMQ.c
@@ -1,16 +1,14 @@
-/* Author : Ashutosh Maurya
-This Program is a part of Minor Project - 1 
-*/
 #include <stdio.h>
 #include <stdbool.h>
 #include <limits.h>
-#define MAX 100 // Let this be maximum number of nodes in our Binary Tree
+#define MAX 1000 
 #define max(a,b) ( (a>b)?a:b )
 #define min(a,b) ( (a<b)?a:b )
 
 int euler[2*MAX - 1]; // Euler Tour Array ( DFS Traversal in a Tree )
-bool visited[MAX+1]; // Level of Nodes in Tour Array
-int first[MAX+1];	// To Store the idx of first occuurence of the node in Euler Tour
+int firstOccur[MAX+1];	// To Store the idx of first occuurence of the node in Euler Tour
+int level[MAX+1];
+bool tree[MAX+1][MAX+1];
 int idx,nodes;
 int segtree[2*MAX - 1];
 
@@ -18,7 +16,7 @@ void initialize(){
 	int i;
 	for(i=0;i<=(2*MAX-2);i++){
 		if(i<=MAX)	
-			first[i]=-1,visited[i]=false;
+			firstOccur[i]=-1;
 		euler[i]=0,segtree[i]=0;
 	}
 }
@@ -49,26 +47,21 @@ int query (int node,int start,int end, int l, int r){
 	return ((x>y)?y:x);
 }
 
-void dfs_eulertour(int node,int l){
-	if(node <= nodes){
-		euler[idx]=node;
-		idx++;
-
-		if(visited[node]==false){
-			visited[node]=true;
-			first[node]=idx-1;
-		}
-		// Left SubTree
-		if(2*node <= nodes){
-			dfs_eulertour(2*node,l+1);
-			euler[idx]=node;
-			idx++;
-		}
-		// Right Subtree
-		if((2*node+1) <= nodes ){
-			dfs_eulertour(2*node+1,l+1);
-			euler[idx]=node;
-			idx++;
+void dfs_eulertour(int current,int previous,int l)
+{
+	if(current<=nodes)
+	{
+		if(firstOccur[current]==-1)
+			firstOccur[current]=idx;
+		euler[idx++]=current;
+		int j;
+		for(j=0;j<=MAX;j++)
+		{
+			if(tree[current][j] && j!=previous)
+			{
+				dfs_eulertour(j,current,l+1);
+				euler[idx++]=current;
+			}
 		}
 	}
 }
@@ -76,21 +69,24 @@ void dfs_eulertour(int node,int l){
 int findLCA(int u,int v){
 	int i;
 	idx=0;
-	dfs_eulertour(1,0);
+	dfs_eulertour(1,0,0);
 	printf("Euler-Tour Array : ");
 	for(i=0;i<(2*nodes-1);i++){
 		printf("%d ",euler[i]);
 	}
 	printf("\n");
 	build(1,0,2*nodes-2); // Building Segment Tree from Euler Tour
-	return query(1,0,2*nodes-2,min(first[u],first[v]),max(first[v],first[u]));
+	return query(1,0,2*nodes-2,min(firstOccur[u],firstOccur[v]),max(firstOccur[v],firstOccur[u]));
 }
 
 int main() {
-	int t,u,v;
-	printf("Enter the number of nodes in the Binary Tree : ");
+	int i,t,u,v;
+	printf("Enter the number of nodes in the Tree : ");
 	scanf("%d",&nodes);
-	printf("Enter the Number of Queries on Binary Tree : ");
+	printf("Enter the Edges in Pairs U V : \n");
+	for(i=0;i<nodes-1;i++)	scanf("%d%d",&u,&v), tree[u][v]=true;
+	//findLCA(1,nodes);
+	printf("Enter the Number of LCA Queries : ");
 	scanf("%d",&t);
 	while(t--){
 		initialize();
